8c09b2cc92db6c4cda3a2204924924a358c94764
nawed2611 <nawed2611@gmail.com>
2024-02-08 12:48:49 +0530

    feat: reset diffusion steps

--- app.py
+++ app.py
@@ -404,7 +404,7 @@ async def tts():
     # save the audio from the URL
     await download_and_save_voice(voiceType)
     voice = compute_style('0.wav')
-    wav = inference(inputText, voice, alpha=0.3, beta=0.7, diffusion_steps=20, embedding_scale=1)
+    wav = inference(inputText, voice, alpha=0.3, beta=0.7, diffusion_steps=5, embedding_scale=1)
     write('result.wav', 24000, wav)
     audio_binary = wav.tobytes()
     
